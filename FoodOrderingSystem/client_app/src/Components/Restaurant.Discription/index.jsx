928b7c41e620b90ee17c00acd48e7f01779cecf1
謝宣鉦 <87351669+Xuan-Zheng-Xie@users.noreply.github.com>
2022-09-24 13:42:05 +0800

    9/23 push

--- FoodOrderingSystem/client_app/src/Components/Restaurant.Discription/index.jsx
+++ FoodOrderingSystem/client_app/src/Components/Restaurant.Discription/index.jsx
@@ -7,7 +7,7 @@ export default class RestaurantDiscription extends Component{
     constructor(props){
         super(props)
         this.state={btn_view:false}
-        this.ChangeBtnLogo = this.ChangeBtnLogo.bind(this)
+        this.ChangeBtnState = this.ChangeBtnState.bind(this)
     }
 
     static propTypes = {
@@ -18,11 +18,17 @@ export default class RestaurantDiscription extends Component{
         Announcement:'一些通知事項\n一些通知事項\n一些通知事項\n一些通知事項\n一些通知事項\n一些通知事項\n一些通知事項\n一些通知事項\n一些通知事項\n一些通知事項\n一些通知事項\n一些通知事項\n一些通知事項\n一些通知事項\n一些通知事項\n一些通知事項\n',
     }
 
-    ChangeBtnLogo(){
+    ChangeBtnState(){
         const btn_view=this.state.btn_view
         this.setState({btn_view:!btn_view})
         console.log(btn_view)
+        
     }
+
+    ChangeAnnouncementHeight(){
+
+    }
+
 
     render(){
         const {Announcement}=this.props
@@ -39,7 +45,7 @@ export default class RestaurantDiscription extends Component{
                         <rect x="0" y="0" width="36" height="36" fillOpacity="0"/>
                     </svg>
                     <span style={{paddingLeft:"8px"}}>提醒事項</span>
-                    <button type="button" className='AnnouncementButton' onClick={this.ChangeBtnLogo}>
+                    <button type="button" className='AnnouncementButton' onClick={this.ChangeBtnState}>
                     {
                             this.state.btn_view === true ?
                             <svg id="AnnouncementButton-Downward-Icon"
@@ -52,13 +58,14 @@ export default class RestaurantDiscription extends Component{
                                 style={{width:"24px",height:"24px"}}
                                 viewBox="0 0 24 24"
                                 xmlns="http://www.w3.org/2000/svg">
-                            <path d="M17,13.41,12.71,9.17a1,1,0,0,0-1.42,0L7.05,13.41a1,1,0,0,0,0,1.42,1,1,0,0,0,1.41,0L12,11.29l3.54,3.54a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29A1,1,0,0,0,17,13.41Z"/>
-                        </svg>
+                                <path d="M17,13.41,12.71,9.17a1,1,0,0,0-1.42,0L7.05,13.41a1,1,0,0,0,0,1.42,1,1,0,0,0,1.41,0L12,11.29l3.54,3.54a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29A1,1,0,0,0,17,13.41Z"/>
+                            </svg>
                         }
                     </button>
+
                 </p>
-                <div className="AnnouncementContent">
-                    <p>{Announcement}</p>
+                <div>
+                    <p className="AnnouncementContent webkit2">{Announcement}</p>
                 </div>
             </div>)
     }
